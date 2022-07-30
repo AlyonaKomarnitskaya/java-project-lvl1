c33a660ef28b782903f3846f31f3ca08cc81a1a1
LenaKomarnitskaya <103958269+LenaKomarnitskaya@users.noreply.github.com>
2022-07-30 05:15:09 +0500

    Update App.java

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -7,6 +7,13 @@ import hexlet.code.games.Prime;
 import java.util.Scanner;
 
 public class App {
+    public static final String GREET_NUM = "1";
+    public static final String EVEN_NUM = "2";
+    public static final String CALC_NUM = "3";
+    public static final String GCD_NUM = "4";
+    public static final String PROGRESSION_NUM = "5";
+    public static final String PRIME_NUM = "6";
+    public static final String EXIT_NUM = "0";
     public static void main(String[] args) {
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet");
@@ -20,25 +27,28 @@ public class App {
         Scanner scanner = new Scanner(System.in);
         String chosenGame = scanner.nextLine();
         switch (chosenGame) {
-            case "1":
+            case GREET_NUM:
                 Cli.greetUser();
                 break;
-            case "2":
-                Even.game();
+            case EVEN_NUM:
+                Even.runGame();
                 break;
-            case "3":
-                Calc.game();
+            case CALC_NUM:
+                Calc.runGame();
                 break;
-            case "4":
-                GCD.game();
+            case GCD_NUM:
+                GCD.runGame();
                 break;
-            case "5":
-                Progression.game();
+            case PROGRESSION_NUM:
+                Progression.runGame();
                 break;
+            case PRIME_NUM:
+                Prime.runGame();
+                break;
+            case EXIT_NUM:
+                System.out.println("Goodbye!");
             default:
-                Prime.game();
-                break;
-
+                System.out.println("Incorrect input!");
         }
     }
 }
